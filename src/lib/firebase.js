5c11a7b2d5950e41eae08577abe291797f8e239c
P. Shanmukh Sai <89543859+Shanmukhsvstp@users.noreply.github.com>
2023-09-24 09:51:25 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: "AIzaSyBEh6qzKnJ2vBbszj5Q4QPuYg0_DkuLk8Q",
+  authDomain: "outstapound.firebaseapp.com",
+  projectId: "outstapound",
+  storageBucket: "outstapound.appspot.com",
+  messagingSenderId: "288334123887",
+  appId: "1:288334123887:web:3e3e4b87cd160c04234e9a",
+  measurementId: "G-HT5DLM16FK"
 };
 
 const firebase = Firebase.initializeApp(config);
